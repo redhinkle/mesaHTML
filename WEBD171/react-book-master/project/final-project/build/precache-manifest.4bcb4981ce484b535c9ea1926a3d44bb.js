b95bb6ffbf5ce3bf4dc0959a061a8375871e5142
Jared Hinkle <jared.h.hinkle@gmail.com>
2020-07-18 09:55:29 -0700

    first commit

---
+++ WEBD171/react-book-master/project/final-project/build/precache-manifest.4bcb4981ce484b535c9ea1926a3d44bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7604d4ef99ce416fc43e0380ce67d644",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a3d7d19f57f999b17f6e",
+    "url": "./static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "bbb5d2471ae2a640292d",
+    "url": "./static/css/main.bfe9a0a2.chunk.css"
+  },
+  {
+    "revision": "a3d7d19f57f999b17f6e",
+    "url": "./static/js/2.a487adc6.chunk.js"
+  },
+  {
+    "revision": "30bfc5a9503c51062fbeb7b3825d08ea",
+    "url": "./static/js/2.a487adc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb5d2471ae2a640292d",
+    "url": "./static/js/main.be505ed8.chunk.js"
+  },
+  {
+    "revision": "c304a19e702054ff1d40",
+    "url": "./static/js/runtime-main.995be748.js"
+  },
+  {
+    "revision": "1321a2d898c5dc12b66154d0ab886bba",
+    "url": "./static/media/colombo.1321a2d8.jpg"
+  }
+]);
